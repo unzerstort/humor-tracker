d310fb28a9ffa20d298754c1e4bba3818e1b71e5
Sandra Bastos <sandra@vermillion.net.br>
2022-06-13 20:50:17 -0300

    Adiciona rota para a tela de login e de dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+
+    component: () => import('../views/DashboardView.vue')
+  },
 ]
 
 const router = new VueRouter({
